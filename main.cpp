1c95b77d8a0834b3840a037a410c49a3f30d46b5
kirusy <ivancerepanov78620@gmail.com>
2024-11-14 22:28:13 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct dog
+{
+    int age;
+    string name;
+    int weight;
+};
+
+void func(int sizes_dogs, const dog *massiv)
+{
+    int maxx = 0;
+    int index_max = 0;
+    for(int i; i < sizes_dogs; i++)
+    {
+        if (maxx < massiv[i].weight)
+        {
+            maxx = massiv[i].weight;
+            index_max = i;
+        }
+    }
+    cout << massiv[index_max].age << endl;
+    cout << massiv[index_max].name << endl;
+    cout << massiv[index_max].weight << endl;
+}
+
+int main()
+{
+    dog labrador = {2, "Cookie", 30};
+    dog spitz = {3, "Bobik", 5};
+    dog husky = {5, "Stiv", 23};
+
+    int sizes_dogs = 3;
+    dog *massiv = new dog[sizes_dogs]{labrador, spitz, husky};
+    func(sizes_dogs, massiv);
+    delete [] massiv;
+    return 0;
+}
